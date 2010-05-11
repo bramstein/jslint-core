0c251db461cb13106c2b0478175d1d2f4d21ba5f
Bram Stein <b.l.stein@gmail.com>
2010-05-11 16:50:06 +0200

    Added script to extract fulljslint.js version number and options and add it to the JSLint module.

---
+++ update.js
@@ -0,0 +1,70 @@
+function getVersion(data) {
+	var m = data.match(/edition = '(.*)'/);
+	if (m && m.length === 2) {
+		return m[1];
+	}
+	return 'unknown';
+}
+
+function getOptions(data) {
+	var optionsStart = data.indexOf('boolOptions = {', 0),
+		optionsEnd = data.indexOf('},', optionsStart),
+		optionsData = data.substring(optionsStart, optionsEnd).split('\n'),
+		options = [];
+
+	optionsData.forEach(function (option) {
+		var m = option.match(/^\s*([A-Za-z_\$0-9]*)\s*:\s*(true|false),?\s*\/\/\s*(.*)\s*$/);
+		if (m) {
+			options.push({
+				name: m[1],
+				value: m[2],
+				description: m[3]
+			});
+		}
+	});
+	return options;
+}
+
+var io = require('file'),
+	data = io.read('lib/fulljslint.js'),
+	packageDescription = {
+		name: "fulljslint",
+		maintainers: [{
+			name: "Douglas Crockford",
+			web: "http://www.crockford.com/"
+		},
+		{
+			name: "Bram Stein",
+			web: "http://www.bramstein.com/",
+			email: "b.l.stein@gmail.com"
+		}],
+		licenses: [{
+			type: "MIT with \"Good not Evil\" clause.",
+			url: "http://www.jslint.com/fulljslint.js"
+		}],
+		homepage: "http://jslint.com/",
+		description: "The JavaScript Code Quality Tool",
+		keywords: ["JavaScript", "lint", "jslint"],
+		version: "2010-04-06"
+	},
+	output, version, options;
+
+if (!data.match('exports.JSLINT =')) {
+	version = getVersion(data);
+	options = getOptions(data);
+	output = io.open('lib/fulljslint.js', 'w');
+
+	output.writeLine(data);
+	output.writeLine('exports.JSLINT = JSLINT;');
+	output.writeLine('exports.options = ' + JSON.stringify(options, null, '\t') + ';');
+	output.writeLine('exports.version = \'' + version + '\';');
+	output.flush();
+
+	// This doesn't meet the CommonJS requirements for version numbers, but we can't
+	// just make up a version number for JSLint. Got to use what we have.
+	packageDescription.version = version;
+
+	output = io.open('package.json', 'w');
+	output.writeLine(JSON.stringify(packageDescription, null, '\t'));
+	output.flush();
+}
--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 wget -O lib/fulljslint.js http://www.jslint.com/fulljslint.js
-echo "exports.JSLINT = JSLINT;" >> lib/fulljslint.js
-NAME=`cat lib/fulljslint.js | grep -o -e "edition = .*" | grep -o [0-9]*-[0-9]*-[0-9]*`
-
-sed -i "s/\"version\": \".*\"/\"version\": \"$NAME\"/g" package.json
+narwhal update.js
