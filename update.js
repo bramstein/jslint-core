afaadd64023ac6879942e37100845aab6425c5c3
Bram Stein <b.l.stein@gmail.com>
2010-09-30 11:06:18 +0200

    Switched jslint-core to Node using npm as package manager.

--- update.js
+++ update.js
@@ -1,3 +1,4 @@
+// FIXME: this is still Narwhal specific
 function getVersion(data) {
 	var m = data.match(/edition = '(.*)'/);
 	if (m && m.length === 2) {
@@ -29,6 +30,7 @@ var io = require('file'),
 	os = require('os'),
 	packageDescription = {
 		name: "jslint-core",
+		author: 'Bram Stein (b.l.stein@gmail.com)',
 		maintainers: [{
 			name: "Douglas Crockford",
 			web: "http://www.crockford.com/"
@@ -42,31 +44,43 @@ var io = require('file'),
 			type: "MIT with \"Good not Evil\" clause.",
 			url: "http://www.jslint.com/fulljslint.js"
 		}],
+        directories: {
+            lib: "./lib"
+        },
+        main: "./lib/jslint-core",
 		homepage: "http://jslint.com/",
 		description: "The JavaScript Code Quality Tool",
 		keywords: ["JavaScript", "lint", "jslint", "jslint-core", "fulljslint"]
 	},
-	output, version, options;
+	output, version, options, oldPackage;
 
 os.command('wget -O lib/jslint-core.js http://www.jslint.com/fulljslint.js');
 
 data = io.read('lib/jslint-core.js');
 
 if (!data.match('exports.JSLINT =')) {
-	version = getVersion(data);
+    oldPackage = JSON.parse(io.read('package.json'));
+    fakeVersion = (oldPackage.version || '1.0.0').split('.');
+	edition = getVersion(data);
 	options = getOptions(data);
 	output = io.open('lib/jslint-core.js', 'w');
+
+    if (oldPackage.edition !== edition) {
+        fakeVersion[1] += 1;
+    }
 
 	output.writeLine(data);
 	output.writeLine('exports.JSLINT = JSLINT;');
 	output.writeLine('exports.options = ' + JSON.stringify(options, null, '\t') + ';');
-	output.writeLine('exports.version = \'' + version + '\';');
+	output.writeLine('exports.edition = \'' + edition + '\';');
+    output.writeLine('exports.version = \'' + fakeVersion.join('.') + '\';');
 	output.flush();
 	output.close();
 
 	// This doesn't meet the CommonJS requirements for version numbers, but we can't
 	// just make up a version number for JSLint. Got to use what we have.
-	packageDescription.version = version;
+	packageDescription.edition = edition;
+    packageDescription.version = fakeVersion.join('.');
 
 	output = io.open('package.json', 'w');
 	output.writeLine(JSON.stringify(packageDescription, null, '\t'));
